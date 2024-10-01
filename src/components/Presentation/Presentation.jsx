d156109009c33d34e2a5dca9d1dc67a22686c9e0
Juan <109253323+bautistaJuan@users.noreply.github.com>
2024-10-01 05:07:37 +0000

    Update esphere size and name text

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -2,10 +2,10 @@ import SphereAnimation from "../bg-anime/Anime";
 export default function Presentation() {
   return (
     <>
-      <main className="min-h-[100vh] flex flex-col justify-center items-center  ">
+      <main className="min-h-[100vh] flex flex-col justify-center items-center lg:flex-row lg:justify-evenly">
         <SphereAnimation />
         <div className="flex flex-col justify-center items-center">
-          <hgroup className="text-[2rem] sm:text-[3rem] md:text-[3rem] text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white w-full">
+          <hgroup className="text-[2rem] mt-[1rem] sm:text-[3rem] text-center lg:text-end border ">
             <h1>HOLA, SOY JUAN.</h1>
             <h2>FRONTEND DEVELOPER</h2>
           </hgroup>
