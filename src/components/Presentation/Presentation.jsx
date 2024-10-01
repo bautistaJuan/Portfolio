e44fcc92956b300bc64235b553bad75ad2889165
Juan <109253323+bautistaJuan@users.noreply.github.com>
2024-10-01 02:12:00 -0300

    Update Presentation.jsx

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -5,7 +5,7 @@ export default function Presentation() {
       <main className="min-h-[100vh] flex flex-col justify-center items-center lg:flex-row lg:justify-evenly">
         <SphereAnimation />
         <div className="flex flex-col justify-center items-center">
-          <hgroup className="text-[2rem] mt-[1rem] sm:text-[3rem] text-center lg:text-end border ">
+          <hgroup className="text-[2rem] mt-[1rem] sm:text-[3rem] text-center lg:text-end">
             <h1>HOLA, SOY JUAN.</h1>
             <h2>FRONTEND DEVELOPER</h2>
           </hgroup>
