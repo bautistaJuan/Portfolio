70e875cbe8cd565c912a6db0af8aa6dbf17174a2
Juan <109253323+bautistaJuan@users.noreply.github.com>
2024-10-04 01:08:16 +0000

    Update Presentation_main-responsive and SphereAnimation-css

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -2,10 +2,10 @@ import SphereAnimation from "../bg-anime/Anime";
 export default function Presentation() {
   return (
     <>
-      <main className="min-h-[100vh] flex flex-col justify-center items-center lg:flex-row lg:justify-evenly">
+      <main className="min-h-[100vh] flex flex-col justify-center items-center lg:flex-row">
         <SphereAnimation />
         <div className="flex flex-col justify-center items-center">
-          <hgroup className="text-[2rem] mt-[1rem] sm:text-[3rem] text-center lg:text-end">
+          <hgroup className="text-[2rem] mt-[1rem] sm:text-[3rem] lg:text-[4rem] text-center ">
             <h1>HOLA, SOY JUAN.</h1>
             <h2>FRONTEND DEVELOPER</h2>
           </hgroup>
